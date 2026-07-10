2c2d126ad3f1b3076afac1ac30c15e68e1ef7b04
govindbohara <85617290+govindbohara@users.noreply.github.com>
2026-07-10 23:17:58 +1000

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    int maxArea = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        for(int row= 0; row<grid.length;row++){
+            for(int col= 0; col<grid[0].length;col++){
+                if(grid[row][col] == 1){
+                    maxArea = Math.max(checkArea(grid, row,col), maxArea);
+                   
+                }
+            }
+        }
+        return maxArea;
+        
+    }
+
+    public int checkArea(int[][] grid, int row , int col){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0){
+            return 0;
+        }
+        int area = 1;
+        grid[row][col] = 0;
+        area += checkArea(grid, row-1, col);
+        area += checkArea(grid, row+1, col);
+        area += checkArea(grid, row, col-1);
+        area += checkArea(grid, row, col+1);
+        return area;
+    }
+
+
+}
